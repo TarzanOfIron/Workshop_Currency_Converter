0234fe83a62dcc4cb2701a3b745dbeb352f71e87
Boti <medgyesi.me@icloud.com>
2025-01-15 11:10:20 +0100

    added the userInterface file and in that added methods that prints lines and takes user input.

---
+++ Workshop_Currency_Converter/src/main/java/org/example/UserInterface.java
@@ -0,0 +1,33 @@
+package org.example;
+
+import java.math.BigDecimal;
+import java.util.Scanner;
+
+public class UserInterface {
+
+    public static void startApp() {
+        printMenu();
+        System.out.println(exchangeOption());
+        System.out.println(amountToExchange());
+    }
+
+    public static void printMenu() {
+        System.out.println("1. Convert SEK to USD");
+        System.out.println("2. Convert USD to SEK");
+        System.out.println("3. Convert SEK to EUR");
+        System.out.println("4. Convert EUR to SEK");
+        System.out.println("0. Exit ");
+    }
+
+    public static int exchangeOption () {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter option: ");
+        return sc.nextInt();
+    }
+
+    public static BigDecimal amountToExchange() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter amount: ");
+        return sc.nextBigDecimal();
+    }
+}
